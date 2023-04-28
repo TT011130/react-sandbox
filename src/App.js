eea8bff9bb20cd208db9286e675d144b5dbf0ded
TT011130 <65943266+TT011130@users.noreply.github.com>
2023-04-28 19:17:06 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+//chạy 1 hàm nếu như mà có trạng thái nào đó thay đổi sẽ kích hoạt render lại
+//dùng để gọi api
+//để chạy 1 hàm sau mỗi lần render
+
+//useEffect:
+//Thực thi các tác vụ chứa side effect
+//Side Effect là các hành động tác động đến hệ thống bên ngoài
